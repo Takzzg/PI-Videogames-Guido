48467d4287f52cfa8c0a5c645edbbec33cb2c529
Guido Queiroz <Takzzg@hotmail.com>
2022-04-25 21:58:49 -0300

    dispatch errors

--- client/src/redux/actions/async.js
+++ client/src/redux/actions/async.js
@@ -15,28 +15,28 @@ export const fetchGames = () => (dispatch) =>
     axios
         .get(`${baseUrl}/videogames`)
         .then((res) => dispatch({ type: FETCH_GAMES, payload: res.data }))
-        .catch((err) => ({ type: ERROR, payload: err }))
+        .catch((err) => dispatch({ type: ERROR, payload: err }))
 
 export const fetchGenres = () => (dispatch) =>
     axios
         .get(`${baseUrl}/genres`)
         .then((res) => dispatch({ type: FETCH_GENRES, payload: res.data }))
-        .catch((err) => ({ type: ERROR, payload: err }))
+        .catch((err) => dispatch({ type: ERROR, payload: err }))
 
 export const fetchDetail = (id) => (dispatch) =>
     axios
         .get(`${baseUrl}/videogame/${id}`)
         .then((res) => dispatch({ type: FETCH_DETAIL, payload: res.data }))
-        .catch((err) => ({ type: ERROR, payload: err }))
+        .catch((err) => dispatch({ type: ERROR, payload: err }))
 
 export const setGames = (name) => (dispatch) =>
     axios
         .get(`${baseUrl}/videogames/?name=${name}`)
         .then((res) => dispatch({ type: SET_GAMES, payload: res.data }))
-        .catch((err) => ({ type: ERROR, payload: err }))
+        .catch((err) => dispatch({ type: ERROR, payload: err }))
 
 export const postGame = (game) => (dispatch) =>
     axios
         .post(`${baseUrl}/videogame`, { ...game })
         .then((res) => dispatch({ type: POST_GAME, payload: res.data }))
-        .catch((err) => ({ type: ERROR, payload: err }))
+        .catch((err) => dispatch({ type: ERROR, payload: err }))
